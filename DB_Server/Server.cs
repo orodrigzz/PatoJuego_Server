58686781b4a90da0894c3db4ced8a7ec3cf5ab45
orodrigzz <oscarrod18503@gmail.com>
2023-05-23 09:36:30 +0200

    Server

--- DB_Server/Server.cs
+++ DB_Server/Server.cs
@@ -1,12 +1,14 @@
 ﻿class Server {
     static void Main(string[] args)
     {
+        //Bool para controlar cuando queremos apagar el servidor
         bool bServerOn = true;
 		
-		//Instanciamos el manager de red en un futuro aqui tendremos tambien la base de datos
+		//Instanciamos managers
         Network_Manager Network_Service = new Network_Manager();
-		
-		//Iniciamos los servicios del servidor, actualmente solo tenemos los servicios de red pero en un futuro tendremos BBDD
+        new Database_Manager();
+
+        //Iniciamos los servicios del servidor
         StartServices();
 
 		//Mantenemos el servidor iniciado con un bucle "infinito"
@@ -18,13 +20,15 @@
 			Network_Service.DisconnectClients();
 			//Comprobamos mensajes
             Network_Service.CheckMessage();
+        }
 
-        }	
-
+        //Init services
         void StartServices()
         {
-			//Iniciamos todos los servicios existentes, en este caso solo el red
+			//Iniciamos todos los servicios existentes
             Network_Service.Start_Network_Service();
+
+            Database_Manager._DATABASE_MANAGER.ConnectDB();
         }
     }
 }
